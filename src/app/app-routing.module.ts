cd837db5e260f28022795e343c55c7074494030c
Ahmad Abdullah <ahmadnofly@gmail.com>
2022-06-26 15:46:16 +0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ProjectsComponent } from './projects/projects.component';
 
-
+const routes: Routes = [
+  {path: 'projects', component: ProjectsComponent}
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
