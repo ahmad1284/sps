49846cd31be6276805cc9ab37b20a086498c6c59
Ahmad Abdullah <ahmadnofly@gmail.com>
2022-07-01 09:14:50 +0300

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:8080/api/v1/projects'
+  baseUrl: 'https://safe-stream-66889.herokuapp.com/api/v1/projects'
 };
 
 /*
